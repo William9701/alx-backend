8913037c29d4703fd0edf83a3b49c3bd4f3ff4c0
Obi William <williamobi818@gmail.com>
2024-01-30 23:59:32 +0100

    Cache modules update

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -25,17 +25,3 @@ class FIFOCache(BaseCaching):
         if key is not None:
             # Use .get() to handle the case when the key doesn't exist
             return self.cache_data.get(key, None)
-
-
-my_cache = FIFOCache()
-my_cache.put("A", "Hello")
-my_cache.put("B", "World")
-my_cache.put("C", "Holberton")
-my_cache.put("D", "School")
-my_cache.print_cache()
-my_cache.put("E", "Battery")
-my_cache.print_cache()
-my_cache.put("C", "Street")
-my_cache.print_cache()
-my_cache.put("F", "Mission")
-my_cache.print_cache()
